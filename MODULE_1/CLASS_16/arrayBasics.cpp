109dbda4b4bc80b558a7d63404fc9fafb1683b35
Mubasshir Ahin <104796739+mubasshirahin@users.noreply.github.com>
2025-03-05 01:06:10 +0600

    16

---
+++ MODULE_1/CLASS_16/arrayBasics.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    // for(auto &u : arr){
+    //     cin>>u;
+    // }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    // for(auto u : arr){
+    //     cout<<u<<" ";
+    // }cout<<endl;
+
+}
