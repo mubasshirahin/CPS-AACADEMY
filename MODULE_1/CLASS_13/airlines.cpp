136f261fe60b6117fb7314791485232832eb2341
Mubasshir Ahin <104796739+mubasshirahin@users.noreply.github.com>
2025-03-05 00:42:48 +0600

    a

--- MODULE_1/CLASS_13/airlines.cpp
+++ MODULE_1/CLASS_13/airlines.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    int a,b;
-	    cin>>a>>b;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
 
-        int x = b/100;
+        int x = b / 100;
 
-        if(b %100 != 0){
+        if (b % 100 != 0)
+        {
             x++;
         }
 
         int ans = x - a;
 
-        if(ans<0){
-            cout<<"0"<<endl;
+        if (ans < 0)
+        {
+            cout << "0" << endl;
         }
-        else cout<<ans<<endl;
-
-	    
-
-
-	}
+        else
+            cout << ans << endl;
+    }
 }
